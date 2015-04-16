502131b65276313fdcb19b7d158eaf209163f96a
Adnan <aaziz@Adnans-MacBook-Pro.local>
2015-04-16 11:07:21 -0700

    Created table views table with dynamic array

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    var programmingLangues = ["PHP", "Java", "Ruby", "Swift"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return programmingLangues.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = programmingLangues[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
